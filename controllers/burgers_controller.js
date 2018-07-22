c285ff3cd07293b46abefce536e972592f4ff975
Rye-Guy <ryanracioppo95@gmail.com>
2018-07-22 19:36:18 -0400

    ughhhhh why u know work, like i wanted

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,7 +10,32 @@ router.get('/', function(req, res){
         console.log(hbsObject);
         res.render("index", hbsObject);
     });
+
 });
 
+router.post('/api/burgers', function(res, req){
+    burgers.insert([
+        'burger_name'
+    ],[
+        req.body.name   
+    ], function(result){
+        console.log(result);
+        res.json(result);   
+        res.redirect('/');
+       
+    }); 
+});
+
+router.put('/api/burgers/:id', function(res, req){
+    var condition = 'id = ' + req.params.id;
+    burger.update({
+        devoured: true
+    }, condition, function(data){
+        res.redirect('/');
+     }
+)
+});
+
+
 // Export 
-module.exports = router; 
+module.exports = router;
