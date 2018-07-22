f3e225f8c7d00bde7c3343500400a1ba574f69fd
Rye-Guy <ryanracioppo95@gmail.com>
2018-07-22 01:03:40 -0400

    server update

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+var handlebars = require("express-handlebars");
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+ var routes = require("./controllers/burgers_controller.js");
+
+ app.use(routes);
+
+ app.listen(PORT, function(){
+     console.log("server on http://localhost:" + PORT);
+ });
